ec1d8cfd7decc56920f83920f3847f9f6a030e7b
ljcanales <ljcanales1404@gmail.com>
2021-07-04 22:05:42 -0300

    .

--- src/util/config.py
+++ src/util/config.py
@@ -9,10 +9,14 @@ class Config:
         self.config_path = config_path
     
     def update_config(self):
+        ''' update configuration, if update_config is true '''
+
         if self.actual_config and self.actual_config['update_config']:
             self.actual_config = read_json_file(self.config_path)
 
-    def is_activate(self, config_name):
+    def is_activated(self, config_name):
+        ''' Check config state given by parameter '''
+
         if self.actual_config and config_name in self.actual_config.keys():
             return self.actual_config[config_name]
         print('ERROR: config_name [{}]'.format(config_name))
