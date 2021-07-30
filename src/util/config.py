03c6c53a45fb5efa71bb6dac9c7108d63c6040dd
ljcanales <ljcanales1404@gmail.com>
2021-07-30 18:43:47 -0300

    .

--- src/util/config.py
+++ src/util/config.py
@@ -2,23 +2,23 @@ from typing import Dict
 from src.util.files import read_json_file
 
 class Config:
-    actual_config: Dict[str, bool] = {}
+    _actual_config: Dict[str, bool] = {}
     config_path: str = ''
 
     def __init__(self, config_path):
-        self.actual_config = read_json_file(config_path)
+        self._actual_config = read_json_file(config_path)
         self.config_path = config_path
     
     def update_config(self) -> None:
         ''' update configuration, if update_config is true '''
 
-        if self.actual_config and self.actual_config['update_config']:
-            self.actual_config = read_json_file(self.config_path)
+        if self._actual_config and self._actual_config['update_config']:
+            self._actual_config = read_json_file(self.config_path)
 
     def is_activated(self, config_name: str) -> bool:
         ''' Check config state given by parameter '''
 
-        if self.actual_config and config_name in self.actual_config.keys():
-            return self.actual_config[config_name]
+        if self._actual_config and config_name in self._actual_config.keys():
+            return self._actual_config[config_name]
         print('ERROR: config_name [{}]'.format(config_name))
         return False
--- src/util/date.py
+++ src/util/date.py
@@ -1,8 +1,9 @@
 import datetime
+from src.constants import time
 
 TIME_ZONE = datetime.timezone(datetime.timedelta(hours=-3)) #Argentina UTC-3
 
 def get_actual_datetime() -> datetime.datetime:
     ''' Returns actual datetime by TIME_ZONE '''
 
-    return datetime.datetime.now().astimezone(TIME_ZONE)
+    return datetime.datetime.now().astimezone(time.TIME_ZONE)
