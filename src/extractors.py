9ea62bb9494f45bcdd238e3381b3e0ccc02c07f6
ljcanales <ljcanales1404@gmail.com>
2021-08-02 21:35:52 -0300

    extractors update

--- src/extractors.py
+++ src/extractors.py
@@ -1,9 +1,9 @@
 import re
 from typing import List
 from .types import Match, Tournament, Team
-from .util.files import read_json_file
 from .constants import file_path
-from .util.date import string_to_datetime, WEEK_DAYS
+from .util.files import read_json_file
+from .util.date import string_to_datetime
 
 def extract_teams(data) -> List[Team]:
     data = re.split('(?:\s*[\w|\s]*final[\w|\s]*[:|-]{1}\s+)|(?:\s*vs.?\s*)|(?:\s*-\s*)|(?:\s*\([\w|\s]*final[\w|\s]*\)\s*)', data)
@@ -20,6 +20,8 @@ def extract_teams(data) -> List[Team]:
 
 def extract_match(data) -> Match:
     mat_data = {}
+    mat_data['tour_id'] = data['tour_id']
+    mat_data['tour_name'] = data['tour_name']
     mat_data['time'] = string_to_datetime(data['fecha'], '%Y-%m-%d %H:%M:%S')
     teams = extract_teams(data['nombre'])
     assert len(teams) == 2, 'extract_match failed'
@@ -28,20 +30,17 @@ def extract_match(data) -> Match:
     mat_data['tv'] = []
     for c in data['canales']:
         mat_data['tv'].append(c['nombre'])
-
     return Match(**mat_data)
 
 def extract_tournament(data) -> Tournament:
     tour_data = {}
     tour_data['id'] = data['id']
     tour_data['name'] = data['nombre']
-    date = string_to_datetime(data["primerFecha"], '%Y-%m-%d %H:%M:%S')
-    tour_data['date'] = date.strftime('%d-%m-%Y')
-    tour_data['day_name'] = WEEK_DAYS[date.weekday()]
+    tour_data['date'] = string_to_datetime(data["primerFecha"], '%Y-%m-%d %H:%M:%S')
     matches = []
     for event in data['eventos']:
+        event['tour_id'] = data['id']
+        event['tour_name'] = data['nombre']
         matches.append(extract_match(event))
     tour_data['matches'] = matches
     return Tournament(**tour_data)
-
-
