9c517eb118165662d2098a05103d200d9d510c49
ljcanales <ljcanales1404@gmail.com>
2021-08-08 19:25:24 -0300

    Add ConfigMixin

--- src/mixins/config.py
+++ src/mixins/config.py
@@ -1,29 +1,30 @@
 from typing import Dict
-from src.util.files import read_json_file
+from src.util.files import read_json_file, write_json_file
 from src.constants import file_path
-from src.types import BotModel, ConfigData
+from src.types import ConfigData
 
-class ConfigMixin(BotModel):
+class ConfigMixin():
     _config: ConfigData
 
     def __init__(self):
         self._config = self.__read_config()
     
     def __read_config(self) -> ConfigData:
+        print('[CONFIG] Reading config file')
         data: Dict = read_json_file(file_path.CONFIG)
         if data:
             return ConfigData(**data)
         else:
-            return ConfigData()
-
+            default_config: ConfigData = ConfigData()
+            write_json_file(default_config, file_path.CONFIG)
+            print('[CONFIG] New config file created')
+            return default_config
     
-    def update(self) -> None:
+    def update_config(self) -> None:
         ''' update configuration, if update_config is true '''
 
         if self._config and self._config.update_config:
             self._config = self.__read_config()
-
-        super().update()
 
     def is_activated(self, config_name: str) -> bool:
         ''' Check config state given by parameter '''
