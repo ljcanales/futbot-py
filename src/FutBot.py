e301686fa9bf4cf8550c96b7069cee5f2c0602d2
ljcanales <ljcanales1404@gmail.com>
2021-08-08 18:17:07 -0300

    Futbot update with mixins

--- src/FutBot.py
+++ src/FutBot.py
@@ -2,31 +2,31 @@
 
 import datetime
 from typing import List
-from src.FutBotSocial.FutBotInstagram import FutBotInstagram
 from src.BannerMaker import BannerMaker
-from src.FutBotSocial.FutBotTwitter import FutBotTwitter
 from src.util import files as fs, metrics, text
 from src.util.date import get_actual_datetime
 import src.constants as constants # keys, paths, uri, etc
 from src.util.config import Config
 
+# mixins
+from src.mixins.social import FutBotInstagramMixin, FutBotTwitterMixin
+
 # TOURNAMENTS CONTROL MODULES
-from src.types import Match
-from src.types import Tournament
+from src.types import Match, Tournament
 from src.API_Sports import API_Sports
 
 #sleeping time
 SLEEP_TIME = 300
 
-class FutBot:
+class FutBot(
+    FutBotTwitterMixin,
+    FutBotInstagramMixin
+):
     ''' FutBot class - manage bot behavior '''
 
     def __init__(self):
-        print("\n\nSTARTING... - " + str(get_actual_datetime()))
+        print("\n\n[FutBot] STARTING... - " + str(get_actual_datetime()))
         self.config = Config(constants.file_path.CONFIG)
-
-        self.futbot_twitter = FutBotTwitter()
-        self.futbot_instagram = FutBotInstagram()
 
         # tournaments info
         self.api_sports = API_Sports(constants.uri.API_MATCHES)
@@ -34,16 +34,16 @@ class FutBot:
 
         self.tour_ids = constants.tour_ids
         self.tournaments: List[Tournament] = []
+        super.__init__()
 
-    def update_bot(self) -> None:
+    def update(self) -> None:
         ''' Handle bot update functions '''
 
         self.config.update_config()
 
         self.update_tournaments()
 
-        self.futbot_twitter.update()
-        self.futbot_instagram.update()
+        super().update()
 
         print("- Update Flag at {}".format(str(get_actual_datetime())))
     
@@ -62,12 +62,12 @@ class FutBot:
                 if res_tour:
                     self.tournaments.append(res_tour)
 
-            if self.futbot_twitter.get_last_tweet_date() < get_actual_datetime().date():
+            if self.tw_get_last_tweet_date() < get_actual_datetime().date():
                 tours_tweeted = 0
                 for tour in self.tournaments:
                     try:
                         if tour and tour.matches:
-                            tour.tweet_id = self.futbot_twitter.tweet_status_lst(text.tour_to_text.full_info_lst(tour))
+                            tour.tweet_id = self.tw_tweet_status_lst(text.tour_to_text.full_info_lst(tour))
                             tours_tweeted += 1
                             print("[TW] Publicando partidos del dia - " + tour.name)
                     except Exception as exception:
@@ -86,22 +86,22 @@ class FutBot:
                         try:
                             print('\n[FutBot] Publicando partido -- {} vs {} --'.format(match.team_1.name, match.team_2.name))
                             match_text = text.match_to_text.full_info(match)
-                            match_text += self.futbot_twitter.get_screen_names([match.team_1.account_id, match.team_2.account_id])
+                            match_text += self.tw_get_screen_names([match.team_1.account_id, match.team_2.account_id])
 
                             banners = self.banner_maker.get_banners(match)
 
                             if self.config.is_activated('tweet_match') and banners:
-                                match.tweet_id = self.futbot_twitter.tweet_status(match_text, banners['horizontal'])
+                                match.tweet_id = self.tw_tweet_status(match_text, banners['horizontal'])
                                 matches_tweeted.append(match)
                             if self.config.is_activated('post_story_match') and banners:
-                                self.futbot_instagram.post_story(banners['vertical'])
+                                self.ig_post_story(banners['vertical'])
                         except Exception as e:
                             print("ERROR: update_tournaments()-(2) e=", e)
 
             if matches_tweeted:
                 metrics.increase_metric(metrics.TWEETED_MATCHES, len(matches_tweeted))
                 if self.config.is_activated('send_match_message'):
-                    self.futbot_twitter.send_match_messages(matches_tweeted)
+                    self.tw_send_match_messages(matches_tweeted)
 
 def get_team_ids(keys_list: List[str]) -> List[str]:
         ''' Returns list containing team_ids for each key in list given by parameter '''
