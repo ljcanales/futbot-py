fbeb0e8ac8d2ee497d16358fc513f9630b1fbb01
ljcanales <ljcanales1404@gmail.com>
2021-11-29 03:43:06 -0300

    add util date function

--- futbot/util/date.py
+++ futbot/util/date.py
@@ -1,7 +1,9 @@
 from datetime import (datetime, timedelta, timezone)
+from re import split
 
 TIME_ZONE = timezone(timedelta(hours=-3)) #Argentina UTC-3
 WEEK_DAYS = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+MONTHS = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
 def get_actual_datetime() -> datetime:
     ''' Returns actual datetime by TIME_ZONE '''
@@ -10,3 +12,12 @@ def get_actual_datetime() -> datetime:
 
 def string_to_datetime(date_string: str, format_string: str) -> datetime:
     return datetime.strptime(date_string, format_string).replace(tzinfo=TIME_ZONE)
+
+def day_month_year_to_datetime(date_string: str) -> datetime:
+    data = split(r"\sdel?\s", date_string)
+    return datetime(
+        year = int(data[2]),
+        month = MONTHS.index(data[1]) + 1,
+        day = int(data[0]),
+        tzinfo = TIME_ZONE
+    )
